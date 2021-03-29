971a1bab7a38b4288dddb66db6a86254da29bc30
Shreyas Bhat Kera <shreykera7@gmail.com>
2021-03-29 17:37:18 +0530

    modify config

--- efficientdet/config.py
+++ efficientdet/config.py
@@ -9,6 +9,8 @@ COCO_CLASSES = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "tr
                 "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
                 "teddy bear", "hair drier", "toothbrush"]
 
+FLIR_CLASSES = ["person", "bicycle", "car"]
+
 colors = [(39, 129, 113), (164, 80, 133), (83, 122, 114), (99, 81, 172), (95, 56, 104), (37, 84, 86), (14, 89, 122),
           (80, 7, 65), (10, 102, 25), (90, 185, 109), (106, 110, 132), (169, 158, 85), (188, 185, 26), (103, 1, 17),
           (82, 144, 81), (92, 7, 184), (49, 81, 155), (179, 177, 69), (93, 187, 158), (13, 39, 73), (12, 50, 60),
